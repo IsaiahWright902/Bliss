1fe3566f5b1b83a55218eed4c5a1bde97e0d29fc
IsaiahWright902 <icwright902@gmail.com>
2020-03-02 14:57:02 -0700

    more fixes

--- server/controllers/TaskController.js
+++ server/controllers/TaskController.js
@@ -1,12 +1,12 @@
 import express from 'express'
 import BaseController from "../utils/BaseController";
 import auth0provider from "@bcwdev/auth0provider";
-import { boardService } from '../services/BoardService'
+import { taskService } from '../services/TaskService'
 
 
 
 //PUBLIC
-export class BoardsController extends BaseController {
+export class TasksController extends BaseController {
   constructor() {
     super("api/boards")
     this.router = express.Router()
@@ -19,21 +19,21 @@ export class BoardsController extends BaseController {
   async create(req, res, next) {
     try {
       req.body.creatorEmail = req.userInfo.email
-      let data = await boardService.create(req.body)
+      let data = await taskService.create(req.body)
       return res.status(201).send(data)
     } catch (error) { next(error) }
   }
 
   async edit(req, res, next) {
     try {
-      let data = await boardService.edit(req.params.id, req.userInfo.email, req.body)
+      let data = await taskService.edit(req.params.id, req.userInfo.email, req.body)
       return res.send(data)
     } catch (error) { next(error) }
   }
 
   async delete(req, res, next) {
     try {
-      await boardService.delete(req.params.id, req.userInfo.email)
+      await taskService.delete(req.params.id, req.userInfo.email)
       return res.send("Successfully deleted")
     } catch (error) { next(error) }
   }
