bd6ba35080bd2fada0e86ce621c1d6fd2b2ce0e3
Alicia Gilfoy <aliciagilfoy@gmail.com>
2020-03-05 15:06:32 -0700

    styled

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -3,7 +3,7 @@
     <img src="./lotuslogo.png" alt />
     <h1 class="text-success display-2">Bliss</h1>
     <h2 class="text-info">Keep Calm | Keep Organized</h2>
-    <h3 class="text-secondary">Welcome to Bliss. Create an account to keep your tasks organized.</h3>
+    <h3 class="text-warning">Welcome to Bliss. Create an account to keep your tasks organized.</h3>
   </div>
 </template>
 
